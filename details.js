42f8c7bcb58bf17aacb5b28667ca425f17513801
ALLU-YT <ameenofficial33@gmail.com>
2024-05-09 16:45:52 +0530

    test_10

--- details.js
+++ details.js
@@ -23,41 +23,41 @@ function slideImage() {
 
 window.addEventListener("resize", slideImage);
 
-// AddOnJs
-// Get all the container elements
-var containers = document.querySelectorAll(".AddOnContainerBox");
+// // AddOnJs
+// // Get all the container elements
+// var containers = document.querySelectorAll(".AddOnContainerBox");
 
-// Add click event listener to each container
-containers.forEach(function (container) {
-  container.addEventListener("click", function () {
-    // Toggle 'selected' class for the clicked container
-    container.classList.toggle("selected");
-    // Check if the container is selected
-    if (container.classList.contains("selected")) {
-      // If selected, change border color to yellow
-      container.style.borderColor = "yellow";
-    } else {
-      // If not selected, revert border color to original
-      container.style.borderColor = "#18578c";
-    }
-  });
-});
+// // Add click event listener to each container
+// containers.forEach(function (container) {
+//   container.addEventListener("click", function () {
+//     // Toggle 'selected' class for the clicked container
+//     container.classList.toggle("selected");
+//     // Check if the container is selected
+//     if (container.classList.contains("selected")) {
+//       // If selected, change border color to yellow
+//       container.style.borderColor = "yellow";
+//     } else {
+//       // If not selected, revert border color to original
+//       container.style.borderColor = "#18578c";
+//     }
+//   });
+// });
 
-// Get all elements with the class 'SizeBox'
-// Get all the container elements
-var SizedBoxContainers = document.querySelectorAll(".SizeBox");
+// // Get all elements with the class 'SizeBox'
+// // Get all the container elements
+// var SizedBoxContainers = document.querySelectorAll(".SizeBox");
 
-// Add click event listener to each container
-SizedBoxContainers.forEach(function (container) {
-  container.addEventListener("click", function () {
-    // Deselect all containers
-    SizedBoxContainers.forEach(function (c) {
-      c.classList.remove("selected");
-      c.style.borderColor = "#18578c";
-    });
+// // Add click event listener to each container
+// SizedBoxContainers.forEach(function (container) {
+//   container.addEventListener("click", function () {
+//     // Deselect all containers
+//     SizedBoxContainers.forEach(function (c) {
+//       c.classList.remove("selected");
+//       c.style.borderColor = "#18578c";
+//     });
 
-    // Select the clicked container
-    container.classList.add("selected");
-    container.style.borderColor = "yellow";
-  });
-});
+//     // Select the clicked container
+//     container.classList.add("selected");
+//     container.style.borderColor = "yellow";
+//   });
+// });
